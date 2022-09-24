dfcb5ba8944e13490f8ace77fe711e2bd34f68ce
root <root@DESKTOP-6SIQDAG.localdomain>
2022-09-24 08:35:45 +0100

    Exercise 1-3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(stringInput) {
+    stringInputArray = stringInput.split("");
+    stringInputArrayReverse = stringInputArray.reverse(); 
+    reverseStringOutput = stringInputArrayReverse.join('')
 
+    return reverseStringOutput;
 };
 
 // Do not edit below this line

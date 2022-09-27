826377042cf2bffd267fe880e96bd12880c6c4fb
SenorPepe92 <adenpetersen92@gmail.com>
2022-09-27 18:53:45 +0100

    Completed exercise 4 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,22 @@
-const removeFromArray = function(array, value) {
-    index = array.indexOf(value); 
-    array.splice(index, 1);
-    return array;
+const removeFromArray = function(inputArray, ...args) {
+    filterElements = args; 
+
+    if (filterElements.length < 2) {
+        index = inputArray.indexOf(filterElements[0]); 
+        newArray = inputArray.splice(index, 1);
+        return inputArray;
+    }
+
+    else if (filterElements.length > 1) {
+
+        filteredArray = inputArray.filter(element => !filterElements.includes(element));
+
+        return filteredArray;
+
+    }    
+    else { 
+        console.log('.....else statement....');
+    }
 };
 
 // Do not edit below this line

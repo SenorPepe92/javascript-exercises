9b0b545065a82ea39ae0101908ee123a957cb0d8
SenorPepe92 <adenpetersen92@gmail.com>
2022-09-29 18:53:56 +0100

    Exercise 6 & 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+// Math.round(num * 10)
 
+const ftoc = function(F) {
+  let  C = (F - 32) * (5/9) ;
+  C = Math.round(C*10)/10
+  return C;
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  let F = 32 + (C * 9/5);
+  F = Math.round(F*10)/10
+  return F;
 };
 
 // Do not edit below this line
@@ -11,3 +17,4 @@ module.exports = {
   ftoc,
   ctof
 };
+
